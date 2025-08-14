0c64862bc946d61c18165f986046d6de056a20a0
shahrukhkhanektamek <sharukhkhanektamek1998@gmail.com>
2025-08-14 16:22:00 +0530

    Some points api

--- src/Components/api.js
+++ src/Components/api.js
@@ -9,13 +9,13 @@ const storage = new MMKV();
 
  
     
-export const apiUrl = () => {
+export const apiUrl = () => { 
 
   // const apiUrl = 'http://192.168.1.61/projects/codediffusion/hindibible/api/';
-  // const apiUrl = 'https://digitalnamo.com/azmal/2025/april/hindibible/api/';
-  const apiUrl = 'http://192.168.1.17/projects/hindibible/api/'; 
+  const apiUrl = 'https://digitalnamo.com/azmal/2025/april/hindibible/api/';
+  // const apiUrl = 'http://192.168.1.17/projects/hindibible/api/'; 
 
-    
+     
   const commurl = apiUrl;    
   const mainUrl = apiUrl+'user/';   
   
