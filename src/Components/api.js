f51fdda34e1b5d01dd7fd7dd43c8e623690c47df
shahrukhkhanektamek <sharukhkhanektamek1998@gmail.com>
2025-12-02 14:34:27 +0530

    fsd

--- src/Components/api.js
+++ src/Components/api.js
@@ -16,7 +16,7 @@ const storage = new MMKV();
 export const socketUrl = 'http://192.168.1.17:3003';
 export const apiUrl = () => {   
   // const apiUrl = 'http://192.168.1.61/projects/codediffusion/hindibible/api/'; 
-  const apiUrl = 'https://sademo.online/demo/codediffusion/hindibible/api/'; 
+  const apiUrl = 'https://sademo.online/codediffusion/hindibible/api/'; 
   // const apiUrl = 'https://digitalnamo.com/azmal/2025/april/hindibible/api/'; 
   // const apiUrl = 'http://192.168.1.25/projects/hindibible/api/'; 
 
